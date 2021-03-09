9d14056d84683e58bd071caf6076da01e67f9ed6
Briefwechsel <78321951+Briefwechsel@users.noreply.github.com>
2021-03-09 22:50:53 +0100

    Update briefmarken.js

--- briefmarken.js
+++ briefmarken.js
@@ -1,42 +1,37 @@
 
 
-if (document.documentElement.clientWidth > 667) {
-  let img;
+let img;
   let currentBriefmarkPos;
   let maxBriefmarke = 13;
+function preload() {
+  possible_briefmarke_images = ["media/stamps/stamp1.png", "media/stamps/stamp2.png", "media/stamps/stamp3.png", "media/stamps/stamp4.png", "media/stamps/stamp5.png", "media/stamps/stamp6.png", "media/stamps/stamp7.png", "media/stamps/stamp8.png", "media/stamps/stamp9.png", "media/stamps/stamp10.png", "media/stamps/stamp11.png", "media/stamps/stamp11.2.png", "media/stamps/stamp12.png"];
+}
 
   function setup() {
     pixelDensity(3.0);
-    var cnv = createCanvas(windowWidth, windowHeight);
-        cnv.parent('sketching-canvas');
-    //pixelDensity.Graphics(windowWidth, windowHeight, P2D);
-    //g.pixelDensity(pixelDensity(2));
+    createCanvas(windowWidth, windowHeight);
     imageMode (CENTER);
-    possible_briefmarke_images = ["media/stamps/stamp1.png", "media/stamps/stamp2.png", "media/stamps/stamp3.png", "media/stamps/stamp4.png", "media/stamps/stamp5.png", "media/stamps/stamp6.png", "media/stamps/stamp7.png", "media/stamps/stamp8.png", "media/stamps/stamp9.png", "media/stamps/stamp10.png", "media/stamps/stamp11.png", "media/stamps/stamp11.2.png", "media/stamps/stamp12.png"];
     currentBriefmarkPos = 0;
   }
 
-  function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-  }
-
-  function mouseClicked() {
-    printNextBriefmark();
-  }
 
   function drawImage(img){
-    img.resize(250,0)
+    img.resize(550, 0)
+
+  var w = img.width, h = img.height;
+  w /= 4; h /= 4;
+  img.width = w; img.height = h;
     image(img, mouseX, mouseY)
   }
 
-  function printNextBriefmark() {
-    currentBriefmarkPos++;
-    if (currentBriefmarkPos == possible_briefmarke_images.length)
+
+function mouseClicked() {
+  //currentBriefmarkPos = 0;
+  currentBriefmarkPos++;
+      if (currentBriefmarkPos == possible_briefmarke_images.length)
       currentBriefmarkPos = 0;
-    
+  
+  //for (let currentBriefmarkPos = 0; currentBriefmarkPos < possible_briefmarke_images.length; currentBriefmarkPos++) { 
     loadImage(possible_briefmarke_images[currentBriefmarkPos], drawImage);
-    //if (possible_briefmarke_images.length > maxBriefmarke){
-      //background(255);
-    //};
-  }
+  
 }
