24eabe889930b391bdcb9a3d6037ad79914bdc09
Briefwechsel <78321951+Briefwechsel@users.noreply.github.com>
2021-03-10 09:30:14 +0100

    Update briefmarken.js

--- briefmarken.js
+++ briefmarken.js
@@ -1,37 +1,32 @@
-
-
+let possible_briefmarke_images;
 let img;
-  let currentBriefmarkPos;
-  let maxBriefmarke = 13;
+let currentBriefmarkPos;
 function preload() {
   possible_briefmarke_images = ["media/stamps/stamp1.png", "media/stamps/stamp2.png", "media/stamps/stamp3.png", "media/stamps/stamp4.png", "media/stamps/stamp5.png", "media/stamps/stamp6.png", "media/stamps/stamp7.png", "media/stamps/stamp8.png", "media/stamps/stamp9.png", "media/stamps/stamp10.png", "media/stamps/stamp11.png", "media/stamps/stamp11.2.png", "media/stamps/stamp12.png"];
 }
 
-  function setup() {
-    pixelDensity(3.0);
-    createCanvas(windowWidth, windowHeight);
-    imageMode (CENTER);
-    currentBriefmarkPos = 0;
-  }
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  pixelDensity(3.0);
+  imageMode (CENTER);
+  currentBriefmarkPos = 0;
+}
 
+function mouseClicked() {
+  printNextBriefmark();
+}
 
-  function drawImage(img){
-    img.resize(550, 0)
-
+function drawImage(img){
+  img.resize(550, 0);
   var w = img.width, h = img.height;
   w /= 4; h /= 4;
   img.width = w; img.height = h;
-    image(img, mouseX, mouseY)
-  }
+  image(img, mouseX, mouseY);
+}
 
-
-function mouseClicked() {
-  //currentBriefmarkPos = 0;
+function printNextBriefmark() {
   currentBriefmarkPos++;
-      if (currentBriefmarkPos == possible_briefmarke_images.length)
-      currentBriefmarkPos = 0;
-  
-  //for (let currentBriefmarkPos = 0; currentBriefmarkPos < possible_briefmarke_images.length; currentBriefmarkPos++) { 
-    loadImage(possible_briefmarke_images[currentBriefmarkPos], drawImage);
-  
+  if (currentBriefmarkPos == possible_briefmarke_images.length)
+    currentBriefmarkPos = 0;
+  loadImage(possible_briefmarke_images[currentBriefmarkPos], drawImage);
 }
